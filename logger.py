72cef2e90685d97408d6cc6a062db090e58bc8f1
NoahLPoteat - Laptop <NoahLPoteat@gmail.com>
2026-03-21 12:53:49 -0400

    Logger and Main.py
    Created a logger, just import and call push_logger() from wherever (as long as it is created in the main file first).

---
+++ logger.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from pathlib import Path
+import io
+
+
+def create_log_file():
+  global log_file 
+  cur_time = datetime.now()
+  time_f = str(cur_time)[:-7].replace(':', '-')
+  
+  path = Path("logs") / (time_f + ".txt")
+
+  log_file = open(path, "w")
+  push_log("Log File made")
+
+def push_log(message: str, is_error: bool = False):
+  post_fix: str = ""
+  cur_time = datetime.now()
+  time_f = str(cur_time)[:-7].replace(':', '-')
+  if is_error:
+    post_fix = "ERROR"
+
+  log_file.write(time_f  + "| " + message + " | " + post_fix + "\n")
+
+def close_logger():
+  log_file.close()
+
+create_log_file()
+push_log("testing logger")
+push_log("Logger Working")
+close_logger()
---
+++ main.py
@@ -0,0 +1,17 @@
+import logger
+
+
+logger.create_log_file()
+logger.push_log("Is this working too?")
+logger.push_log("YES IT IS", True)
+
+
+
+
+
+# main loop here or something
+# make the game here 
+
+
+
+logger.close_logger()
