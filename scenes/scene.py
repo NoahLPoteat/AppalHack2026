0940a340365b0ae1d57ed81ecd858a17eff9323a
Dahlia <otheroption67@gmail.com>
2026-03-21 17:17:12 -0400

    changes how choices work

--- scenes/scene.py
+++ scenes/scene.py
@@ -1,3 +1,5 @@
+import json
+
 class scene:
     name = ""
     bg = ""
@@ -12,12 +14,31 @@ class scene:
             "chars": self.characters,
             "lines": self.lines
         }
-    
     def play(self):
         pass
-
 
 class dialogue:
     type = "" #text, choice, sprite
     text = ""
     speaker = "" #name on text box, and highlighting sprites
+    choices = []
+
+
+class choice:
+    text = ""
+    next_scene = ""
+    transition = False
+    affections = "c+1, j-1"
+    
+
+def loadJSON(pathname):
+    with open(pathname, "r") as json_data:
+        d = json.loads(json_data.read())
+        json_data.close()
+    print(d)
+    sc = scene(d["name"])
+    sc.bg = d["bg"]
+    sc.characters = d["chars"]
+    sc.lines = d["lines"]
+    return sc
+    
